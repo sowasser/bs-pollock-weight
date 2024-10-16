0fee1cf7694f23797094e88ca2aa4c1bfea0bec0
Sophia Wassermann <hi@sowasser.com>
2024-10-16 10:43:06 -0700

    Updated formatting

--- scripts/1_data_organization.R
+++ scripts/1_data_organization.R
@@ -1,12 +1,12 @@
-#####ORGANIZE AND FORMAT OBSERVATION DATA
+##### ORGANIZE AND FORMAT OBSERVATION DATA
 
 library(dplyr)
 library(ggplot2)
-library(VAST)
+# library(VAST)
 library(here)
 
-###CPUE
-##Upload data files
+### CPUE ----------------------------------------------------------------------
+## Upload data files
 cruise <- read.csv(here("data", "raw_data", "cruise_data.csv"))
 
 alk <- read.csv(here("data", "data_2024", "age_length_key_full_densdep_corrected2024.csv"))
@@ -20,154 +20,167 @@ names(haul) <- tolower(names(haul))
 names(catch) <- tolower(names(catch))
 names(cruise) <- tolower(names(cruise))
 
-###CPUE data
-##Merging CPUE with *all* cruise data, retaining zero catches
+## Merging CPUE with *all* cruise data, retaining zero catches
 
-#Streamline haul data data_sub to just relevant columns
-
+# Streamline haul data data_sub to just relevant columns
 hauls <- merge(catch, haul, by = c("hauljoin", "cruisejoin")) %>%
-  select(hauljoin, cruisejoin, cruise.x, haul.x, start_latitude, start_longitude, year, weight, number_fish)
+  select(hauljoin, cruisejoin, cruise.x, haul.x, start_latitude, start_longitude,
+         year, weight, number_fish)
 colnames(hauls)[3:4] <- c("cruise", "haul")
 
-#Merge hauls and cpue data to get all hauls w/ zero pollock also
+# Merge hauls and cpue data to get all hauls w/ zero pollock also
 cpue2 <- alk %>%
   full_join(hauls, by= c("cruisejoin", "haul", "year", "start_latitude", "start_longitude"))
 
-#Age bin
+# Age bin
 cpue2$age_bin <- ifelse(cpue2$age>=15, 15, cpue2$age)
 
-#Streamlined data frame
-cpue_final2 <- cpue2[,c("year", "age","cruisejoin", "haul", "hauljoin", "start_latitude", "start_longitude", "age_bin", "age_cpue_corr", "weight")]
+# Streamlined data frame
+cpue_final2 <- cpue2[,c("year", "age","cruisejoin", "haul", "hauljoin", 
+                        "start_latitude", "start_longitude", "age_bin", 
+                        "age_cpue_corr", "weight")]
 
-#Make a column adding the cpue for the 15+ ages together
-cpue_final3 <- cpue_final2%>% 
+# Make a column adding the cpue for the 15+ ages together
+cpue_final3 <- cpue_final2 %>% 
   group_by(year, age_bin, start_latitude, start_longitude) %>%
   mutate(age_cpue_sum = sum(age_cpue_corr)) %>%
   ungroup()
 
-#Filter out duplicate rows w/ multiple 15 age bins
-cpue_final <- cpue_final3%>% 
+# Filter out duplicate rows w/ multiple 15 age bins
+cpue_final <- cpue_final3 %>% 
   group_by(year, age_bin, start_latitude, start_longitude) %>%
   distinct(age_cpue_sum,.keep_all=TRUE) %>%
   ungroup()
 
-#Sanity check that this  is correct and actually does what I want it to
+# Sanity check that this  is correct and actually does what I want it to
 cpue_final3_15 <- subset(cpue_final3, cpue_final3$age_bin==15)
-cpue_final_15b <- cpue_final3_15%>% 
+cpue_final_15b <- cpue_final3_15 %>% 
   group_by(year, age_bin, start_latitude, start_longitude) %>%
   distinct(age_cpue_sum,.keep_all=TRUE) %>%
   ungroup()
-#PASSES
+#PASSES - SNW: how to tell? 
 
-#Remove hauls w/ biomass measure but no abundance-at-age from dataset
-cpue_final$abundance_yn <- ifelse( !is.na(cpue_final$age_cpue_corr), 1, 0)
-cpue_final$zero_biomass_yn <- ifelse(cpue_final$weight==0, 1,0)
-cpue_final$biomass_na_yn <- ifelse(is.na(cpue_final$weight), 1,0)
+# Remove hauls w/ biomass measure but no abundance-at-age from dataset
+cpue_final$abundance_yn <- ifelse(!is.na(cpue_final$age_cpue_corr), 1, 0)
+cpue_final$zero_biomass_yn <- ifelse(cpue_final$weight == 0, 1, 0)
+cpue_final$biomass_na_yn <- ifelse(is.na(cpue_final$weight), 1, 0)
 
-cpue_final_edit <- subset(cpue_final, cpue_final$abundance_yn==1|cpue_final$zero_biomass_yn==1)
-cpue_final_removed <- subset(cpue_final, cpue_final$abundance_yn!=1&cpue_final$zero_biomass_yn!=1)
+cpue_final_edit <- subset(cpue_final, cpue_final$abundance_yn == 1|cpue_final$zero_biomass_yn == 1)
+cpue_final_removed <- subset(cpue_final, cpue_final$abundance_yn != 1 & cpue_final$zero_biomass_yn != 1)
 
-#Isolate hauls w/ a biomass measure but no abundance-at-age
-missing_abundance <- subset(cpue_final, cpue_final$age_cpue_corr ==0 & cpue_final$weight>0)
-missing_abundance <- subset(missing_abundance, missing_abundance$year !=2021)
-missing_abundance <- subset(missing_abundance, missing_abundance$age_cpue_corr !=0)
+# Isolate hauls w/ a biomass measure but no abundance-at-age
+missing_abundance <- subset(cpue_final, cpue_final$age_cpue_corr == 0 & cpue_final$weight > 0)
+missing_abundance <- subset(missing_abundance, missing_abundance$year != 2021)
+missing_abundance <- subset(missing_abundance, missing_abundance$age_cpue_corr != 0)
 
-#save intermediate steps as csv, if wanted
+# save intermediate steps as csv, if wanted
 write.csv(cpue_final_removed, here("data", "formatted_data", "missing_abundance.csv"))
 write.csv(cpue_final, here("data", "formatted_data", "cpue_final_no_zero_corrected.csv"))
 
-#save intermediate steps as rds, if wanted
+# save intermediate steps as rds, if wanted
 saveRDS(cpue_final_removed, here("data", "formatted_data", "missing_abundance.rds"))
 saveRDS(cpue_final, here("data", "formatted_data", "cpue_final_no_zero_corrected.rds"))
 
-#Remove weird age zeroes
+# Remove weird age zeroes
 cpue_final_edit2 <- cpue_final_edit %>% 
-  filter(age!=0|is.na(age))
+  filter(age != 0 | is.na(age))
 
-##Add zeroes for hauls missing some age classes
+## Add zeroes for hauls missing some age classes
 # Sanity check
 # Check that there's some zeros
 Data=cpue_final_edit2
 Data <- as.data.frame(Data)
-out = tapply( Data[,'age_cpue_sum'], INDEX=list(Data[,'year'],Data[,'age_bin']), FUN=function(x){sum(x==0)} )
+out <- tapply(Data[,'age_cpue_sum'], INDEX = list(Data[,'year'], Data[,'age_bin']), 
+              FUN = function(x){sum(x == 0)})
 # FAILS CHECK
 
 # Sanity check
 # Check that sample size is equal for all ages in a given year
-out2 = tapply( Data[,'age_cpue_sum'], INDEX=list(Data[,'year'],Data[,'age_bin']), FUN=length )
+out2 <- tapply(Data[, 'age_cpue_sum'], INDEX = list(Data[, 'year'], Data[, 'age_bin']), 
+               FUN=length)
 # FAILS CHECK
 
 # Sanity check
 # Check that each hauljoin has 1-15 observations
-table(table(Data[,'hauljoin']))
+table(table(Data[, 'hauljoin']))
 # PASSES CHECK
 
-#Make column numeric
+# Make column numeric
 Data$age_cpue_sum <- as.numeric(Data$age_cpue_sum)
 
-Data_fix = tapply( Data[,'age_cpue_sum'], INDEX=list("hauljoin"=Data[,'hauljoin'],"age_bin"=Data[,'age_bin']), FUN=sum )
-Data_fix = ifelse( is.na(Data_fix), 0, Data_fix )
+Data_fix <- tapply(Data[,'age_cpue_sum'], INDEX = list("hauljoin"=Data[, 'hauljoin'], 
+                                                       "age_bin"= Data[, 'age_bin']), 
+                   FUN = sum)
+Data_fix <- ifelse(is.na(Data_fix), 0, Data_fix)
 
 # rebuild wide-form data frame
-Data_wide = cbind( Data_fix, Data[match(rownames(Data_fix),Data[,'hauljoin']),c("start_latitude","start_longitude","year")] )
+Data_wide <- cbind(Data_fix, Data[match(rownames(Data_fix), Data[, 'hauljoin']), 
+                                  c("start_latitude", "start_longitude", "year")])
 
 # build long-form data frame
-Data_long = expand.grid( dimnames(Data_fix) )
-Data_long = cbind( Data_long, "age_cpue_sum"=as.vector(Data_fix), Data[match(Data_long[,'hauljoin'],Data[,'hauljoin']),c("start_latitude","start_longitude","year")] )
+Data_long <- expand.grid(dimnames(Data_fix))
+Data_long <- cbind(Data_long, "age_cpue_sum" = as.vector(Data_fix), 
+                   Data[match(Data_long[, 'hauljoin'], Data[, 'hauljoin']),
+                        c("start_latitude", "start_longitude", "year")])
 
 # Sanity check
 # Check that there's some zeros
-out = tapply( Data_long[,'age_cpue_sum'], INDEX=list(Data_long[,'year'],Data_long[,'age_bin']), FUN=function(x){sum(x==0)} )
+out <- tapply(Data_long[,'age_cpue_sum'], INDEX = list(Data_long[, 'year'],
+                                                       Data_long[, 'age_bin']), 
+              FUN = function(x){sum(x == 0)} )
 # PASSES CHECK
 
 # Sanity check
 # Check that sample size is equal for all ages in a given year
-out2 = tapply( Data_long[,'age_cpue_sum'], INDEX=list(Data_long[,'year'],Data_long[,'age_bin']), FUN=length )
+out2 <- tapply( Data_long[, 'age_cpue_sum'], INDEX=list(Data_long[, 'year'], 
+                                                        Data_long[, 'age_bin']), 
+                FUN = length )
 # PASSES CHECK
 
 glimpse(Data_long)
 
-#Remove age zeros
-Data_long <- subset(Data_long, Data_long$age_bin !=0)
+# Remove age zeros
+Data_long <- subset(Data_long, Data_long$age_bin != 0)
 
-#Save final cpue data
+# Save final cpue data
 write.csv(Data_long, here("data", "formatted_data", "final", "cpue_final.csv"))
 saveRDS(Data_long, here("data", "formatted_data", "final", "cpue_final.rds"))
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-####SPECIMEN DATA (IE WEIGHT)
 
-#Determine False/True if weight data available
+#### SPECIMEN DATA (IE WEIGHT) ------------------------------------------------
+
+# Determine False/True if weight data available
 specimen$weight_available <- specimen$weight %>% 
   is.na %>% 
   `!` 
-#Determine False/True if age data available
+# Determine False/True if age data available
 specimen$age_available <- specimen$age%>% 
   is.na %>% 
   `!` 
-#Designate categories 1, 2, 3, 4 for level of data available
-specimen$data_available <- case_when(specimen$weight_available==TRUE & specimen$age_available==FALSE~2,
-                                     specimen$age_available==TRUE & specimen$weight_available==FALSE~3,
-                                     specimen$age_available==FALSE & specimen$weight_available==FALSE~1,
-                                     specimen$age_available==TRUE & specimen$weight_available==TRUE~4)
+# Designate categories 1, 2, 3, 4 for level of data available 
+specimen$data_available <- case_when(specimen$weight_available == TRUE & specimen$age_available == FALSE ~ 2,
+                                     specimen$age_available == TRUE & specimen$weight_available == FALSE  ~ 3,
+                                     specimen$age_available == FALSE & specimen$weight_available == FALSE ~ 1,
+                                     specimen$age_available == TRUE & specimen$weight_available == TRUE ~ 4)
 
-#Make 15+ bin in new variable called "age_bin"
-specimen$age_bin <- ifelse(specimen$age>=15, 15, specimen$age)
+# Make 15+ bin in new variable called "age_bin"
+specimen$age_bin <- ifelse(specimen$age >= 15, 15, specimen$age)
 
-#Merge with haul information
-cruise$cruisejoin <- as.factor(cruise$cruisejoin)
-specimen$cruisejoin <- as.factor(specimen$cruisejoin)
+# Merge with haul information
+# cruise$cruisejoin <- as.factor(cruise$cruisejoin)
+# specimen$cruisejoin <- as.factor(specimen$cruisejoin)
 
-#Merge  dataframes and make new dataframe
+# Merge dataframes and make new dataframe
 data2 <- specimen %>% 
   left_join(cruise, by = "cruisejoin")
 
-#Create column w/ EBS and NBS based on stratum
-data2$location <- ifelse(data2$stratum==81 | data2$stratum==70 |data2$stratum==71,'northern','eastern')
+# Create column w/ EBS and NBS based on stratum
+data2$location <- ifelse(data2$stratum == 81 | data2$stratum == 70 |data2$stratum == 71,
+                         'northern', 'eastern')
 
-#####Calculate weights from weight-length relationship w=aL^b
-#Using parameters from Jim Ianelli's spreadsheet
-#1=male, 2=female
+##### Calculate weights from weight-length relationship w=aL^b
+# Using parameters from Jim Ianelli's spreadsheet
+# 1=male, 2=female
 B_m <- 3.038
 B_f <- 2.986
 B_unknown <- 2.9954
@@ -175,43 +188,51 @@ A_m <- 0.000004919
 A_f <- 0.000006681
 A_unknown <- 0.0000063611
 
-data2$weight_calculated <- case_when(data2$sex==1 ~A_m*data2$length^B_m,
-                                     data2$sex==2 ~A_f*data2$length^B_f,
-                                     data2$sex==3~A_unknown*data2$length^B_unknown)
+data2$weight_calculated <- case_when(data2$sex == 1 ~ A_m * data2$length ^ B_m,
+                                     data2$sex == 2 ~ A_f  *data2$length ^ B_f,
+                                     data2$sex == 3 ~ A_unknown * data2$length ^ B_unknown)
 
-#Compare weights, if wanted
-plot(data2$weight_calculated~data2$weight, ylab="Calculated weight (w=aL^b)", xlab="Measured weight")
-abline(a=0,b=1)
+# Compare weights, if wanted
+plot(data2$weight_calculated ~ data2$weight, 
+     ylab = "Calculated weight (w=aL^b)", 
+     xlab = "Measured weight")
+abline(a = 0,b = 1)
 abline(lm(weight_calculated ~ weight, data = data2), col = "blue")
 
-#Save intermediate step as csv, if wanted
+# Save intermediate step as csv, if wanted
 write.csv(data2, file = here("data", "formatted_data", "specimen_data_edited_complete.csv"))
 
-#Create streamlined dataframe for individual weight data
-specimen_data <- data2[,c('year', 'start_latitude', 'start_longitude', 'region.x','survey_name', 'location','hauljoin', 'cruisejoin', 'sex', 'age_bin', 'age', 'weight', 'weight_calculated','length', 'data_available')]
+# Create streamlined dataframe for individual weight data
+specimen_data <- data2[, c('year', 'start_latitude', 'start_longitude', 
+                           'region.x','survey_name', 'location','hauljoin', 
+                           'cruisejoin', 'sex', 'age_bin', 'age', 'weight', 
+                           'weight_calculated','length', 'data_available')]
 
-#Save as csv and rds
+# Save as csv and rds
 write.csv(specimen_data, file = here("data", "formatted_data", "specimen_data_edited_streamlined.csv"))
 saveRDS(specimen_data, file = here("data", "formatted_data", "specimen_data_edited_streamlined.rds"))
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#####Handling extrapolation grid
-#Add ebs and nbs extrapolation grid csv's from Jim Thorson
-ebs_grid <- read.csv("~/Dropbox/Mac/Documents/Documents/Pollock/data/EBSThorsonGrid.csv")
-nbs_grid <- read.csv("~/Dropbox/Mac/Documents/Documents/Pollock/data/NBSThorsonGrid.csv")
 
-#Add columns for "eastern" and "northern"
-ebs_grid$location <- paste("eastern")
-nbs_grid$location <- paste("northern")
+##### Handling extrapolation grid ---------------------------------------------
+# #Add ebs and nbs extrapolation grid csv's from Jim Thorson
+# ebs_grid <- read.csv("~/Dropbox/Mac/Documents/Documents/Pollock/data/EBSThorsonGrid.csv")
+# nbs_grid <- read.csv("~/Dropbox/Mac/Documents/Documents/Pollock/data/NBSThorsonGrid.csv")
+# 
+# #Add columns for "eastern" and "northern"
+# ebs_grid$location <- paste("eastern")
+# nbs_grid$location <- paste("northern")
+# 
+# #Merge grids
+# bs_grid <- rbind(ebs_grid, nbs_grid)
+# 
+# #Convert m^2 to km^2 and label column w/ correct VAST formatting
+# bs_grid$Area_km2 <- bs_grid$Shape_Area/1000000
+# 
+# #Save extrapolation grid as rds file
+# saveRDS(bs_grid, file="bs_grid.rds")
 
-#Merge grids
-bs_grid <- rbind(ebs_grid, nbs_grid)
-
-#Convert m^2 to km^2 and label column w/ correct VAST formatting
-bs_grid$Area_km2 <- bs_grid$Shape_Area/1000000
-
-#Save extrapolation grid as rds file
-saveRDS(bs_grid, file="bs_grid.rds")
+# Read in extrapolation grid
+bs_grid <- readRDS(here("data", "formatted_data", "final", "bs_grid.rds"))
 
 ####Combine CPUE and specimen (weight) data for model fitting
 #Upload CPUE and specimen data, if needed
